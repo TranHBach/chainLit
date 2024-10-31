48c46ae9bdbfb8d9c0082d08d01486f15428178b
TranHBach <ragoza0932447933@gmail.com>
2024-10-31 09:18:36 +0700

    First commit

---
+++ setup.py
@@ -0,0 +1,41 @@
+import os
+import openai
+
+openai.api_key = os.environ.get("OPENAI_API_KEY")
+import nest_asyncio
+
+nest_asyncio.apply()
+
+
+from llama_index.readers.file import UnstructuredReader
+from pathlib import Path
+
+years = [2022, 2021, 2020, 2019]
+
+loader = UnstructuredReader()
+doc_set = {}
+all_docs = []
+for year in years:
+    year_docs = loader.load_data(
+        file=Path(f"./data/UBER/UBER_{year}.html"), split_documents=False
+    )
+    # insert year metadata into each year
+    for d in year_docs:
+        d.metadata = {"year": year}
+    doc_set[year] = year_docs
+    all_docs.extend(year_docs)
+
+# initialize simple vector indices
+from llama_index.core import VectorStoreIndex, StorageContext
+from llama_index.core import Settings
+
+Settings.chunk_size = 512
+index_set = {}
+for year in years:
+    storage_context = StorageContext.from_defaults()
+    cur_index = VectorStoreIndex.from_documents(
+        doc_set[year],
+        storage_context=storage_context,
+    )
+    index_set[year] = cur_index
+    storage_context.persist(persist_dir=f"./storage/{year}")
