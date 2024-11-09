6e92344b3c7e0d654d8742cf321e664c3a226d43
TranHBach <ragoza0932447933@gmail.com>
2024-11-10 01:00:36 +0700

    add password

--- app.py
+++ app.py
@@ -30,6 +30,17 @@ from llama_index.agent.openai import OpenAIAgent
 
 agent = OpenAIAgent.from_tools(tools, verbose=True)
 
+@cl.password_auth_callback
+def auth_callback(username: str, password: str):
+    # Fetch the user matching username from your database
+    # and compare the hashed password with the value stored in the database
+    if (username, password) == ("admin", "admin"):
+        return cl.User(
+            identifier="admin", metadata={"role": "admin", "provider": "credentials"}
+        )
+    else:
+        return None
+
 
 @cl.on_chat_start
 def start_chat():
